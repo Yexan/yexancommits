28095c3acdb1dbe96314824a8cda4eb4f6364833
adrien <contact.adrien.martinet@gmail.com>
2019-08-27 16:00:11 +0200

    Update handle commit to override email account

--- handlenewcontent.sh
+++ handlenewcontent.sh
@@ -1,7 +1,12 @@
 echo 'Handle new content'
 
+old_git_mail=`git config user.email`
+git config user.email contact.adrien.martinet@gmail.com
+
 git add --all
 git commit -m 'Add new gifs'
 git push
+
+git config user.email $old_git_mail
 
 echo 'New GIF pushed'
